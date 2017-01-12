ab83bd4d226b557ad9884b4715ea229103477483
Misha Polishchyk <mishaelitzem2@rambler.ru>
2017-01-12 13:55:31 +0200

    SSSSs9

--- webpage/settings.py
+++ webpage/settings.py
@@ -26,7 +26,7 @@ with open('webpage/secret_key.txt') as f:
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = False
+DEBUG = True
 
 # DEBUG = bool(os.environ.get('DJANGO_DEBUG', False))
 
